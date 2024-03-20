8841b0d58f91ba60b23b84411b1869eb3b6a411e
AzharM <Gr33nMayhem@gmail.com>
2024-03-20 13:02:58 -0400

    Added a bash script to run training on ComputeCanada

---
+++ run_scripts/bash_jobs/train.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --account=def-plago
+#SBATCH --export=ALL,DISABLE_DCGM=1
+#SBATCH --gpus-per-node=v100:1
+#SBATCH --mem=64G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --time=48:0:0
+#SBATCH --mail-user=Gr33nMayhem@gmail.com
+#SBATCH --mail-type=ALL
+
+# Wait until DCGM is disabled on the node
+while [ ! -z "$(dcgmi -v | grep 'Hostengine build info:')" ]; do
+  sleep 5;
+done
+cd ~/projects/def-plago/akhaked/SyntheticIMUGeneration/run_scripts
+module purge
+module load python/3.11 scipy-stack
+source ~/py311/bin/activate
+python train_encoder_decoder.py
